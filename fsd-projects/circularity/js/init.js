2deec67d0568c98d4ef4a2fe1d37003ed7479b0a
JaxonDenais1489948 <jjd1489948@lpssonline.com>
2026-04-01 14:47:36 +0000

    done

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -85,7 +85,7 @@ for (var loopsCompleted = 0; loopsCompleted < 175; loopsCompleted++) {
         */
         function update() {
             // TODO 4 : Update the position of each circle using physikz.updatePosition()
-            for (var i = 0; i < circles.length; i++) {
+       for (var i = 0; i < circles.length; i++) {
 
                physikz.updatePosition(circles[i]);
 
@@ -95,15 +95,7 @@ for (var loopsCompleted = 0; loopsCompleted < 175; loopsCompleted++) {
 }
             
             // TODO 5 : Call game.checkCirclePosition() on your circles
-            game.checkCirclePosition(circles[ 0 ])
-
-            game.checkCirclePosition(circles[ 1 ])
-
-            game.checkCirclePosition(circles[ 2 ])
-
-            game.checkCirclePosition(circles[ 3 ])
-
-            game.checkCirclePosition(circles[ 4 ])
+            
 
             // TODO 8 / TODO 9 : Iterate over the array
            
